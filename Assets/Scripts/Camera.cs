ed10dd04f633f9d5bd99a98e93e48a8636206f9e
Sean <seanfkh@hotmail.com>
2019-05-14 19:16:48 -0700

    Modified UI elements

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -6,11 +6,10 @@ namespace myCamera
     {
         private void Awake()
         {
-            //Disables all unnecessary script icons in the Scene view.
 #if UNITY_EDITOR
-            CustomEditorUtilities.ToggleGizmos(false);
+            CustomEditorUtilities.ToggleGizmos(false); //Disables all unnecessary script icons in the Scene view.
 #endif
-            
+
             Screen.SetResolution(1920, 1080, true); //Ensures the proper resolution is set when the game is built and run.
             Cursor.visible = false;
         }
